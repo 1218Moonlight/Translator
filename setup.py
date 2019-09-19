8f34e4b6bb37c5809b8f0670b350421479fb7cce
kioryu <1218moonlight@gmail.com>
2019-09-19 12:05:05 +0900

    [ADD] setup.py

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup_requires = []
+
+# conda install tensorflow
+install_requires = [
+    # 'django==1.6b4',
+    'numpy',
+    'pandas',
+    'keras'
+]
+
+dependency_links = [
+    # 'git+https://github.com/django/django.git@stable/1.6.x#egg=Django-1.6b4',
+]
+
+print('Translator modules\n%s' % install_requires)
+
+setup(name='Translator',
+      version='1.0',
+      description='Translator services',
+      author='Kioryu',
+      author_email='1218Moonlight@gmail.com',
+      packages=find_packages(),
+      install_requires=install_requires,
+      setup_requires=setup_requires,
+      dependency_links=dependency_links,
+      scripts=[],
+      entry_points={}
+      )
